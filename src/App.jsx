a0cf35296fcc7eb94f348ff01a53e52526806868
BenMoreira <bmoreira@ggc.edu>
2023-08-01 14:13:34 -0400

    Adds font and some text

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,17 @@ function App() {
   return (
     <div className='w-full min-h-screen bg-[url("/leopard.jpg")] py-1'>
       <div className='w-[95%] min-h-[99vh] bg-[url("/flowers.jpg")] bg-cover mx-auto border-1 rounded-lg'>
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center text-3xl font-bold gap-2'>
           <div>
-            
+            It's my 50th Birthday Party
+          </div>
+
+          <div>
+            and
+          </div>
+
+          <div>
+          YOU are invited
           </div>
         </div>
       </div>
