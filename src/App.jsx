5d07de02d5d274d973eb2b0c3b1f47c9c102f70d
BenMoreira <bmoreira@ggc.edu>
2023-08-01 21:04:23 -0400

    Adds pcitures and more info

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='w-full min-h-screen bg-[url("/leopard.jpg")] py-1'>
-      <div className='w-[95%] min-h-[99vh] bg-[url("/flowers.jpg")] bg-cover mx-auto border-1 rounded-lg'>
-        <div className='flex flex-col justify-center items-center text-3xl font-bold gap-2'>
-          <div>
-            It's my 50th Birthday Party
-          </div>
+      <div className='w-[95%] min-h-[99vh] bg-[url("/flowers.jpg")] bg-cover mx-auto border-1 rounded-lg pt-4'>
+        <div className='flex flex-col justify-center items-center text-4xl font-bold gap-4'>
+          <img src='/flowerline.png' />
 
           <div>
-            and
+            It's my 50th Birthday Party!!
           </div>
 
           <div>
           YOU are invited
           </div>
+        </div>
+
+        <div className='flex flex-col justify-center items-center text-2xl gap-2 pt-10'>
+          <div className='font-bold text-3xl'>
+            When and Where:
+          </div>
+
+          <div>
+            Saturday, August 12th
+          </div>
+          
+          <div>
+            3896 Egypt Rd. Snellville, GA, 30039
+          </div>
+
+          <div>
+            @ 7 P.M. - 10 P.M.
+          </div>
+
+          <img src='/leopardpic.png' className='w-[300px]' />
         </div>
       </div>
     </div>
